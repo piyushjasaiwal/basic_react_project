e0a72686307fe59fa2d821cfd10043be353d77f8
Piyush Jasaiwal <coderpiyush123@gmail.com>
2020-11-15 02:20:36 +0530

    started with react poject

---
+++ social card/social/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import images from'./components/right_image'
+
+function App() {
+  return (
+    <div className="App">
+      <images />
+    </div>
+  );
+}
+
+export default App;
